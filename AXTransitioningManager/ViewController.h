eeeb7150cb064f51c41948390bb531d1e89ad960
aiXing <862099730@qq.com>
2016-07-17 18:52:02 +0800

    Initial Commit

---
+++ AXTransitioningManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXTransitioningManager
+//
+//  Created by devedbox on 16/7/17.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
